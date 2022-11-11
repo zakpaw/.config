b166c2583cd27107a12bf994e8247d3abba1b0a8
Pawel Zak <zpawe@amazon.com>
2022-11-11 20:13:12 +0100

    add missing

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,1 +1,8 @@
+require("catppuccin").setup({
+    flavour = "mocha",
+    styles = {
+        comments = { "italic" },
+    }
+})
+
 vim.cmd.colorscheme "catppuccin"
---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,26 @@
+require("nvim-treesitter.configs").setup({
+    highlight = { enable = true },
+    indent = { enable = true },
+    autotag = { enable = true },
+    ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "markdown",
+        "svelte",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "python",
+        "rust",
+        "sql",
+        "hcl"
+    },
+    auto_install = true,
+})
