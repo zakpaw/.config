372e0403b816daa93441cf22e3699b30cf969f77
Pawel Zak <zpawe@amazon.com>
2022-11-13 12:56:49 +0100

    one esc telescope close

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -12,6 +12,7 @@ telescope.setup({
   defaults = {
     mappings = {
       i = {
+        ["<esc>"] = actions.close, -- use single esc to close
         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
         ["<C-j>"] = actions.move_selection_next, -- move to next result
         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
