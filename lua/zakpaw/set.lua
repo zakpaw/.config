719dd50eb7d6b8fd3352e6e07c85c408c708c90b
Pawel Zak <zpawe@amazon.com>
2022-11-13 16:58:59 +0100

    change to 80%

--- lua/zakpaw/set.lua
+++ lua/zakpaw/set.lua
@@ -32,5 +32,5 @@ vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 2
 vim.g.netrw_preview = 1
-vim.g.netrw_winsize = 70
+vim.g.netrw_winsize = 80
 vim.g.netrw_keepdir = 0
