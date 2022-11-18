973da482ff6b517f5f666ccfee57be62df89c297
Pawel Zak <zpawe@amazon.com>
2022-11-18 19:25:26 +0100

    auto indent on save

--- lua/zakpaw/set.lua
+++ lua/zakpaw/set.lua
@@ -39,3 +39,6 @@ vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_keepdir = 0
 vim.g.netrw_winsize = -25
+
+-- autocmd
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
