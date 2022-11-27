a55fd6085693101186ed8bfee3cbd9d23f10528a
Pawel Zak <zpawe@amazon.com>
2022-11-27 15:08:24 +0100

    transparent background

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,8 +1,25 @@
 require("catppuccin").setup({
+    transparent_background = false,
     flavour = "mocha",
     styles = {
         comments = { "italic" },
     }
 })
-
 vim.cmd.colorscheme "catppuccin"
+
+-- transparent background
+local transparent_elements = {
+    "Normal",
+    "NormalNC",
+    "LineNr",
+    "Folded",
+    "NonText",
+    "SpecialKey",
+    "VertSplit",
+    "SignColumn",
+    "EndOfBuffer",
+    "CursorLine",
+}
+for _, elem in pairs(transparent_elements) do
+    vim.cmd("hi "..elem.." guibg=NONE ctermbg=NONE")
+end
