50f3c0e233e5719c378d922ad154ee8a17112a96
Pawel Zak <zpawe@amazon.com>
2022-12-20 17:03:39 +0100

    <cmd> to :

--- lua/zakpaw/remap.lua
+++ lua/zakpaw/remap.lua
@@ -1,7 +1,7 @@
 -- basic normal
-vim.keymap.set("n", "<leader>q", "<cmd>bd<cr>")
-vim.keymap.set("n", "<leader>e", "<cmd>Ex<cr>")
-vim.keymap.set("n", "<leader>w", "<cmd>topleft vs<cr>|<cmd>Ex<cr>")
+vim.keymap.set("n", "<leader>q", ":bd<cr>")
+vim.keymap.set("n", "<leader>e", ":Ex<cr>")
+vim.keymap.set("n", "<leader>w", ":topleft vs<cr>|:Ex<cr>")
 vim.keymap.set("n", "<s-H>", "^")
 vim.keymap.set("n", "<s-L>", "$")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -11,30 +11,30 @@ vim.keymap.set("n", "N", "Nzz")
 vim.keymap.set("n", "x", '"_x')
 vim.keymap.set("n", "<leader>=", "<C-a>") -- increment number
 vim.keymap.set("n", "<leader>-", "<C-x>") -- decrement number
-vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>") -- next bufferrema
-vim.keymap.set("n", "<S-Tab>", "<cmd>bprev<cr>") -- previous buffer
+vim.keymap.set("n", "<Tab>", ":bnext<cr>") -- next bufferrema
+vim.keymap.set("n", "<S-Tab>", ":bprev<cr>") -- previous buffer
 
 -- basic visual
-vim.keymap.set("v", "<s-J>", "<cmd>m '>+1<cr>") -- move line down
-vim.keymap.set("v", "<s-K>", "<cmd>m '<-2<cr>") -- move line up
+vim.keymap.set("v", "<s-J>", ":m '>+1<CR>gv=gv") -- move line down
+vim.keymap.set("v", "<s-K>", ":m '<-2<CR>gv=gv") -- move line up
 vim.keymap.set("v", "<s-H>", "^")
 vim.keymap.set("v", "<s-L>", "$")
 
 -- telescope
-vim.keymap.set("n", "<C-f>", "<cmd>Telescope find_files<cr>")
-vim.keymap.set("n", "<C-s>", "<cmd>Telescope find_files hidden=true<cr>")
-vim.keymap.set("n", "<C-g>", "<cmd>Telescope live_grep<cr>")
-vim.keymap.set("n", "<C-b>", "<cmd>Telescope git_branches theme=dropdown<cr>")
-vim.keymap.set("n", "<C-p>", "<cmd>Telescope neoclip plus theme=dropdown initial_mode=normal<cr>")
+vim.keymap.set("n", "<C-f>", ":Telescope find_files<cr>")
+vim.keymap.set("n", "<C-s>", ":Telescope find_files hidden=true<cr>")
+vim.keymap.set("n", "<C-g>", ":Telescope live_grep<cr>")
+vim.keymap.set("n", "<C-b>", ":Telescope git_branches theme=dropdown<cr>")
+vim.keymap.set("n", "<C-p>", ":Telescope neoclip plus theme=dropdown initial_mode=normal<cr>")
 
 -- lspconfig
-vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions theme=dropdown<cr>")
-vim.keymap.set("n", "gD", "<cmd>Telescope lsp_references theme=dropdown<cr>")
-vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
+vim.keymap.set("n", "gd", ":Telescope lsp_definitions theme=dropdown<cr>")
+vim.keymap.set("n", "gD", ":Telescope lsp_references theme=dropdown<cr>")
+vim.keymap.set("n", "K", ":lua vim.lsp.buf.hover()<cr>")
 
 -- netrw
 vim.keymap.set("n", "<nop>", "<buffer><Plug>NetrwRefresh") -- stop netrw from refreshing at c-l
 
 -- other
-vim.keymap.set("n", "<F5>", "<cmd>UndotreeToggle<cr>")
-vim.keymap.set("n", "<C-e>", "<cmd>lua vim.diagnostic.open_float({'line'})<cr>")
+vim.keymap.set("n", "<F5>", ":UndotreeToggle<cr>")
+vim.keymap.set("n", "<C-e>", ":lua vim.diagnostic.open_float({'line'})<cr>")
